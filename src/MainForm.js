8aba791b69601ce12ce1c6114b8f26ee3e376fde
kenpachi <itwabi@gmail.com>
2021-02-15 19:28:44 +0200

    code cleanUp and added a datePicker to the layout instead of separate selects for year, month and week

--- src/MainForm.js
+++ src/MainForm.js
@@ -12,33 +12,16 @@ import {
     MDBCol
 } from "mdbreact";
 import Select from 'react-select'
-import makeAnimated from 'react-select/animated';
 import NavBar from "./NavBar";
 import {getInstance} from "d2";
 
 
-const animatedComponents = makeAnimated();
 const { RangePicker } = DatePicker;
 const weeks = [
     { value: 'week-1', label: 'Week 1' },
     { value: 'week-2', label: 'Week 2' },
     { value: 'week-3', label: 'Week 3' },
     { value: 'week-4', label: 'Week 4' },
-]
-
-const months = [
-    { value: '01', label: 'January' }, { value: '02', label: 'February' },
-    { value: '03', label: 'March' }, { value: '04', label: 'April' },{ value: '05', label: 'May' },
-    { value: '06', label: 'June' }, { value: '07', label: 'July' },{ value: '08', label: 'August' },
-    { value: '09', label: 'September' },{ value: '10', label: 'October' },{ value: '11', label: 'November' },
-    { value: '12', label: 'December' },
-]
-
-const years = [
-    { value: '2021', label: '2021' },
-    { value: '2020', label: '2020' },
-    { value: '2019', label: '2019' },
-    { value: '2018', label: '2018' },
 ]
 
 var moment = require("moment");
@@ -49,14 +32,9 @@ const MainForm = (props) => {
     const [orgUnits, setOrgUnits] = useState([]);
     const [programs, setPrograms] = useState([]);
     const [searchValue, setSearchValue] = useState([]);
-    const [instances, setInstances] = useState([]);
     const [selectedOrgUnit, setOrgUnit] = useState([]);
-    const [selectedMonth, setSelectedMonth] = useState(null);
     const [selectedProgram, setSelectedProgram] = useState(null);
-    const [selectedWeek, setSelectedWeek] = useState(null);
-    const [selectedInstance, setSelectedInstance] = useState(null);
     const [events, setEvents] = useState(null);
-    const [selectedYear, setSelectedYear] = useState(null);
     const [startDate, setStartDate] = useState(null);
     const [endDate, setEndDate] = useState(null);
 
@@ -103,42 +81,11 @@ const MainForm = (props) => {
     const onSelect = (value, node) => {
         setOrgUnit(selectedOrgUnit => [...selectedOrgUnit, node]);
         console.log(node);
-        /*
-        var id = "edb4aTWzQaZ"
-        getInstance().then((d2) => {
-            const endpoint = `trackedEntityInstances.json?paging=false&ou=${id}`
-            d2.Api.getApi().get(endpoint)
-                .then((response) => {
-                    console.log(response.trackedEntityInstances)
-
-                    const tempArray = []
-                    response.trackedEntityInstances.map((item, index) => {
-                        tempArray.push({"id" : item.trackedEntityInstance, "label" : item.trackedEntityInstance})
-                    });
-                    setInstances(tempArray);
-                })
-                .catch((error) => {
-                    console.log("error: " + error);
-            })
-        });
-         */
     };
 
     const handleProgram = selectedOption => {
         setSelectedProgram(selectedOption);
     };
-    const handleWeek = selectedOption => {
-        setSelectedWeek(selectedOption);
-    };
-
-    const handleMonth = selectedOption => {
-        setSelectedMonth(selectedOption);
-        console.log(selectedOption)
-    };
-
-    const handleYear = selectedOption => {
-        setSelectedYear(selectedOption);
-    }
 
     const test = () => {
         console.log(startDate);
@@ -148,16 +95,12 @@ const MainForm = (props) => {
     const handleLoadEvents = () => {
 
         console.log(selectedOrgUnit);
-        console.log(selectedWeek.value);
         console.log(selectedProgram);
-        console.log(selectedMonth);
 
-        var week = selectedWeek.value;
-        var month = selectedMonth[0].value;
         var progID = selectedProgram.id;
         var orgID = selectedOrgUnit[0].id;
         //var trackedID = selectedInstance[0].id;
-        console.log(week, month, progID, orgID);
+        console.log( progID, orgID);
 
         var id = "edb4aTWzQaZ"
         getInstance().then((d2) => {
